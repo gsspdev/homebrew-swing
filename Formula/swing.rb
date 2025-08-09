578c99394f0b91d7dec58ec94a9ab90a500adc0f
gsspdev <dev@gssp.io>
2025-08-09 16:59:07 -0500

    Create swing.rb

---
+++ Formula/swing.rb
@@ -0,0 +1,32 @@
+# Homebrew Formula for Jazz Standards Database CLI
+class Swing < Formula
+  desc "CLI tool for searching and analyzing jazz standards with chord progressions"
+  homepage "https://github.com/gsspdev/swing"
+  url "https://github.com/gsspdev/swing/archive/v1.0.0.tar.gz"
+  sha256 "997c7f16e3c7aa9e79410145aabadcf90ea9c90da07aacd91ea31f5c0302a23e"
+  license "MIT"
+
+  depends_on "rust" => :build
+
+  def install
+    system "cargo", "install", *std_cargo_args
+  end
+
+  test do
+    # Test that the binary runs and shows version
+    assert_match "swing", shell_output("#{bin}/swing --version")
+    
+    # Test basic functionality
+    output = shell_output("#{bin}/swing stats")
+    assert_match "Total songs:", output
+    assert_match "Jazz Standards Database Statistics", output
+    
+    # Test search functionality
+    search_output = shell_output("#{bin}/swing search 'miles davis'")
+    assert_match "Found", search_output
+    
+    # Test help system
+    help_output = shell_output("#{bin}/swing --help")
+    assert_match "CLI tool for searching and analyzing", help_output
+  end
+end
